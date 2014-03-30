5480aa5376807defef302b0daae9b74fff7e25fd
Ozcan Tercan <otercan88@gmail.com>
2014-03-31 00:06:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YeniyuvaApp::Application.config.secret_key_base = '3677cae7aca79d3046ee0bb2056a69b1b12fd72e4c13a17a46f59e4e6e483e51047c3764b66812e7147205bace3a85ad27a889f6386c7fae86fc71aaba66ef2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YeniyuvaApp::Application.config.session_store :cookie_store, key: '_yeniyuva_app_session'
