6d6a7ca792802e68588516c14d131a556dc1a448
Ozcan Tercan <otercan88@gmail.com>
2014-03-31 01:37:20 +0100

    Replace security token code Update README Install rspec

---
+++ README.md
@@ -0,0 +1,5 @@
+# yeniyuva property advertising application
+
+This is the ruby on rails application for the property advertising company yeni yuva.
+
+Initial Developer : Ozcan Tercan
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-YeniyuvaApp::Application.config.secret_key_base = '3677cae7aca79d3046ee0bb2056a69b1b12fd72e4c13a17a46f59e4e6e483e51047c3764b66812e7147205bace3a85ad27a889f6386c7fae86fc71aaba66ef2c'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+YeniyuvaApp::Application.config.secret_key_base = secure_token
